12791a36cda06a0d0def40f277a6bfa5281660f4
BacroXP <135527354+BacroXP@users.noreply.github.com>
2026-07-14 10:17:07 +0200

    Add: concatenation-of-array - submission-0

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-0.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int[] ret = new int[nums.length * 2];
+
+        for (int i = 0; i < nums.length; i++) {
+            ret[i] = nums[i];
+            ret[i + nums.length] = nums[i];
+        }
+
+        return ret;
+    }
+}
